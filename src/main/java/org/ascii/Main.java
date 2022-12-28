3133e23b80e9b091faef245b0b8692268f091483
Helal Anwar <hilal123anwar@gmail.com>
2022-12-29 00:07:27 +0530

    Castling and EnPassant Implemented

--- src/main/java/org/ascii/Main.java
+++ src/main/java/org/ascii/Main.java
@@ -1,11 +1,15 @@
 package org.ascii;
 
 import org.ascii.chess.Game;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import org.jline.utils.InfoCmp;
 
+import java.io.IOException;
+
 public class Main {
-    static String TITLE= """
-            
+    static String TITLE = """
+                        
             ░█████╗░░██████╗░█████╗░██╗██╗  ░█████╗░██╗░░██╗███████╗░██████╗░██████╗
             ██╔══██╗██╔════╝██╔══██╗██║██║  ██╔══██╗██║░░██║██╔════╝██╔════╝██╔════╝
             ███████║╚█████╗░██║░░╚═╝██║██║  ██║░░╚═╝███████║█████╗░░╚█████╗░╚█████╗░
@@ -13,18 +17,25 @@ public class Main {
             ██║░░██║██████╔╝╚█████╔╝██║██║  ╚█████╔╝██║░░██║███████╗██████╔╝██████╔╝
             ╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝╚═╝  ░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
             """;
+
     public static void main(String[] args) throws InterruptedException {
         loading(new StringBuilder("=>"));
         Game game = new Game();
         game.start();
     }
+
     private static void loading(StringBuilder s) throws InterruptedException {
-        while (s.length() <= 45) {
-            System.out.println(TITLE.indent(30));
-            s.insert(0, "=");
-            System.out.println(s.toString().indent(25));
-            Thread.sleep(50);
-            System.out.println("\033[H\033[J");
+
+        try (Terminal terminal = TerminalBuilder.terminal()) {
+            while (s.length() <= 80) {
+                terminal.puts(InfoCmp.Capability.clear_screen);
+                System.out.println(TITLE.indent(30));
+                s.insert(0, "=");
+                System.out.println(s.toString().indent(25));
+                Thread.sleep(16);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
